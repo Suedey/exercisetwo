8d27fb97dd0e12f8c1360f20b99f6814f83ff6f5
Suedey <Ot2017@nyu.edu>
2023-09-19 09:34:13 -0400

    Class 09.19.23

--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -1,9 +1,10 @@
+import ArticleCard from "../../app/Components/ArticleCard";
 
 export default function Article(){
     return (
         <main>
             <h1>Article</h1>
-            <p>testing</p>
+            <ArticleCard title ="Cool Title" />
         </main>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import Data from '../app/Components/data'
+import ArticleCard from '../app/Components/ArticleCard';
 
 export default function Home(){
     const projectName = "Exercise Two"
 
-    console.log(Data);
     return (
         <main>
             <h1>{projectName}</h1>
+            <ArticleCard title="Cool Title" description={"Cool Description"}/>
         </main>
     );
 }
