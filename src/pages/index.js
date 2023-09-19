1612dc3a42b530cc71d92d46f65bc226d595e52f
Suedey <Ot2017@nyu.edu>
2023-09-19 09:15:50 -0400

    V2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
-import Counter from "../app/components/TestingCounter";
+import Data from '../app/Components/data'
 
 export default function Home(){
     const projectName = "Exercise Two"
+
+    console.log(Data);
     return (
         <main>
             <h1>{projectName}</h1>
-            <Counter />
         </main>
     );
 }
